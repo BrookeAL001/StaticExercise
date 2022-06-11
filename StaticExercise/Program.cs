b390ff6b79cf09ea92726cc66303bf47d28b4cbc
BrookeAL0012 <bbcecil17@gmail.com>
2022-06-11 12:03:11 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var celsius = TempConverter.FarenheitToCelsius(68);
+
+            Console.WriteLine($"Celsius {celsius}");
+
+            var farenheit = TempConverter.CelsiusToFarenheit(20);
+
+            Console.WriteLine($"Farenheit {farenheit}");
+
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FarenheitToCelsius (double farenheit)
+        {
+            var result = (farenheit - 32) / 1.8;
+            return result;
+        }
+
+        public static double CelsiusToFarenheit(double celsius)
+        {
+            var result = (celsius * 9) / 5 + 32;
+            return result;
+        }
+    }
+}
